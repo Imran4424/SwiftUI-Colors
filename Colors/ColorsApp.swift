65dde7de24d2e175b9f4809953b8b72c2bc2d756
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-02 21:25:42 +0600

    complex colors

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Shah Md Imran Hossain on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colors/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Colors
+//
+//  Created by Shah Md Imran Hossain on 2/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            
+            Text("SwiftUI Color Magic")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
